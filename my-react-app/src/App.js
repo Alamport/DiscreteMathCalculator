f870300cf53fbbce14879a4718c5eebfd567410a
AdinLamport <108032137+Alamport@users.noreply.github.com>
2024-02-27 16:22:41 -0500

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p><i>I have to learn quickly</i></p>
         </p>
         <a
           className="App-link"
